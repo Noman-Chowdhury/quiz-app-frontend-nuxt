834b1eed09ca39f3bebc2fb884d93270d5153d7e
Noman-Chowdhury <orkib420@gmail.com>
2022-06-09 14:51:57 +0600

    dist

---
+++ dist/_nuxt/static/1654764637/admin/question/create/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/admin/question/create", {data:[{}],fetch:{},mutations:[]});
---
+++ dist/_nuxt/static/1654764637/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Flogin","\u002Fregister","\u002Fadmin\u002Fquestion\u002Fcreate"]})
