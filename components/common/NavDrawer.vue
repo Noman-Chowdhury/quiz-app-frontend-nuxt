b1faeee32c5f6a8808421eed1f0fd170078edb00
Md.Eshan <mdeshan307@gmail.com>
2022-08-03 09:01:10 +0600

    Header and Navbar added

---
+++ components/common/NavDrawer.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-navigation-drawer
+    v-model="drawer"
+  >
+    <v-list>
+
+      <v-list-item link>
+        <v-list-item-avatar>
+          <v-img src="https://randomuser.me/api/portraits/women/85.jpg"></v-img>
+        </v-list-item-avatar>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6">
+            Sandra Adams
+          </v-list-item-title>
+          <v-list-item-subtitle>sandra_a88@gmail.com</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list
+      nav
+      dense
+    >
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-folder</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>My Files</v-list-item-title>
+      </v-list-item>
+      <v-list-item link>
+        <v-list-item-icon>
+          <v-icon>mdi-account-multiple</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Shared with me</v-list-item-title>
+      </v-list-item>
+      <v-list-item link @click.prevent="logout">
+        <v-list-item-icon>
+          <v-icon>mdi-logout</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Logout</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "NavDrawer",
+  data(){
+    return {
+      drawer:true
+    }
+  },
+  methods: {
+    logout() {
+      this.$auth.logout()
+    },
+  },
+}
+</script>
+
+<style scoped>
+</style>
